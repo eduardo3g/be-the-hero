bc2426f6b25e5d856fa4ef498106d78cdce694d2
= <=>
2020-03-24 19:59:28 -0300

    feat: Posting ONGs on sqlite.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.get('/healthcheck', (req, res) => {
-  return res.json({
-    healthcheck: 'The app is alive! :D'
+routes.post('/ongs', async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
   });
+
+  return res.json({ id });
 });
 
 module.exports = routes;
