d424b2153f3627709a5fd9bff3d234ee945550d3
= <=>
2020-03-29 14:05:54 -0300

    chore: Formatted donation value to PT-BR currency and showed incident count on header.

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -11,6 +11,7 @@ import styles from './styles';
 
 export default function Incidents() {
   const [incidents, setIncidents] = useState([]);
+  const [total, setTotal] = useState(0);
 
   const navigation = useNavigation();
 
@@ -22,6 +23,7 @@ export default function Incidents() {
     const response = await api.get('/incidents');
 
     setIncidents(response.data);
+    setTotal(response.headers['x-incident-count']);
   }
 
   useEffect(() => {
@@ -33,7 +35,7 @@ export default function Incidents() {
       <View style={styles.header}>
         <Image source={logoImg} />
         <Text style={styles.headerText}>
-          Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+          Total de <Text style={styles.headerTextBold}>{total} casos</Text>.
         </Text>
       </View>
 
@@ -54,7 +56,12 @@ export default function Incidents() {
             <Text style={styles.incidentValue}>{incident.title}</Text>
 
             <Text style={styles.incidentProperty}>VALOR:</Text>
-            <Text style={styles.incidentValue}>{incident.value}</Text>
+            <Text style={styles.incidentValue}>
+              {Intl.NumberFormat('pt-br', { 
+                style: 'currency',
+                currency: 'BRL'
+              }).format(incident.value)}
+            </Text>
 
             <TouchableOpacity
               style={styles.detailsButton}
